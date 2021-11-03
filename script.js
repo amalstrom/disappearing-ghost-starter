5211c868537a895a2ff6424ac8287fe8769ca7b2
Glitch (indecisive-excellent-parcel) <none>
2021-11-03 22:27:22 +0000

    🌌💍 Updated with Glitch

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+$(".hide-button").click(function() {
+  $(".ghost-image").hide()
+});
 
+$(".show-button").click(function() {
+  $(".ghost-image").show()
+});
+
+$(".update-img-button").click(function() {
+  $(".ghost-image").attr("src", "https://www.ama.org/wp-content/uploads/2019/01/ghost-ads-image.jpg")
+});
+
+$(".message-button").click(function() {
+  let userInput = $(".input").val();
+  $(".message").append(userInput)
+});
+
+$(".name-button").click(function() {
+  let userInput = $(".input").val();
+  $("h1").text("Hey fam, my name is " + userInput + "!")
+});
